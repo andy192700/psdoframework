66adec51de98e3238ad92e763756805499c8963c
andy192700 <ahughes2311@gmail.com>
2025-02-04 17:28:44 +0000

    add notes to git release

--- Scripts/GitRelease.ps1
+++ Scripts/GitRelease.ps1
@@ -1,3 +1,4 @@
+using namespace System.Text;
 using module ".\lib\VersionCalculator.psm1";
 
 [string] $version = [VersionCalculator]::GetLatest("PSGallery");
@@ -9,5 +10,11 @@ git config --global user.email "github-actions@github.com";
 git tag -a $version -m "Release $version";
 git push origin --tags;
 
-gh release create $version --title "Release $version" --notes "Check out the release notes at https://github.com/andy192700/psdoframework/tree/main/Documentation/ReleaseNotes/v$($version).md.";
+[StringBuilder] $sb = [StringBuilder]::new();
+
+$sb.AppendLine("Regarding this release:");
+$sb.AppendLine("- Check out the [Release Notes](https://github.com/andy192700/psdoframework/tree/main/Documentation/ReleaseNotes/v$($version).md)");
+$sb.AppendLine("- View the published Module at the [PowerShell Gallery](https://www.powershellgallery.com/packages/PSDoFramework/$version)");
+
+gh release create $version --title "Release $version" --notes $sb.ToString();
     
