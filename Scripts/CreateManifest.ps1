f899b8664980eaa73300effe010f2b5c4690d283
andy192700 <ahughes2311@gmail.com>
2025-02-15 23:30:11 +0000

    reinstate nested modules.....

--- Scripts/CreateManifest.ps1
+++ Scripts/CreateManifest.ps1
@@ -36,6 +36,14 @@ Copy-Item -Path "$rootDir$($sep)src$($sep)DoFramework$($sep)DoFramework$($sep)bi
 
 [FileSystemInfo[]] $items = @();
 
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)FileSystem" -Recurse;
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)Mappers" -Recurse;
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)Modules" -Recurse;
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)Processing" -Recurse;
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)Validators" -Recurse;
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)Services$($sep)ApplicationServiceContainer.psm1";
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)CLI" -Recurse;
+$items += Get-ChildItem "$baseDir$($sep)Objects$($sep)Services$($sep)CLIFunctionServiceContainer.psm1";
 $items += Get-ChildItem "$baseDir$($sep)Functions";
 
 [string] $manifestPath = "$buildDir$($sep)PSDoFramework.psd1";
